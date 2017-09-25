b71f1dee59a1f158b771d1d2c31e49953c2d2ea7
Claus Wiingreen <cwiing13@student.aau.dk>
2017-09-25 09:43:08 +0200

    Added Report Template

---
+++ make.bat
@@ -0,0 +1,4 @@
+pdflatex -synctex=1 master.tex
+bibtex master.aux
+pdflatex -synctex=1 master.tex
+pdflatex -synctex=1 master.tex
---
+++ makelive.bat
@@ -0,0 +1,1 @@
+latexmk -pvc -pdflatex="pdflatex -file-line-error -synctex=1" -pdf master.tex 
